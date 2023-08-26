68e3e6cc7449a2365bdbc2793cdad9dcd835f2a6
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-08-26 23:43:14 +0200

    Debug protocol refactored

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build/micro-utils/test/micro_utils_test
-./build/firmware/control-panel/test/control_panel_test
-./build/firmware/line-detector/test/line_detector_test
+./build/micro-utils/test/micro_utils_test && \
+./build/firmware/control-panel/test/control_panel_test && \
+./build/firmware/line-detector/test/line_detector_test && \
 ./build/firmware/motor-controller/test/motor_controller_test
